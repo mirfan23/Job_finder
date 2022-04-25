51bc87c9d95ff3816dd1b8d5f16d584a2b29ae60
Muhammad Irfan Fadhila <irfanfadhila000@gmail.com>
2022-04-25 08:47:39 +0700

    add font and home

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,14 +8,52 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      // appBar: AppBar(
+      //   title: Text('HomeView'),
+      //   centerTitle: true,
+      // ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(
+                          Get.width,
+                        ),
+                        child: Image(
+                          image: AssetImage(
+                            'assets/images/girl-vektor.jpg',
+                          ),
+                          height: 50,
+                          width: 50,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Hello \nOrlando Diggs.",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                        Container(),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
